032083f04b9a55cea51692f6476bc4cb8be445ae
Nicolas Ormeno <nicolasormeno@hotmail.com>
2023-06-27 11:27:43 -0300

    cambios js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let usuarios = [
         contrasena: 'prueba',
         dni: 45234442,
         rutaImagen: 'profesora.jpg',
+        sexo: 'Femenino',
     },
     {
         legajo: 2,
@@ -16,6 +17,7 @@ let usuarios = [
         contrasena: 'prueba',
         dni: 42330393,
         rutaImagen: 'profesora.jpg',
+        sexo: 'Femenino',
     },
     {
         legajo: 3,
@@ -25,6 +27,7 @@ let usuarios = [
         contrasena: 'prueba',
         dni: 43982710,
         rutaImagen: 'profesor.jpg',
+        sexo: 'Masculino',
     },
     {
         legajo: 4,
@@ -34,6 +37,7 @@ let usuarios = [
         contrasena: 'prueba',
         dni: 44992692,
         rutaImagen: 'profesora.jpg',
+        sexo: 'Femenino',
     },
     {
         legajo: 5,
@@ -43,6 +47,7 @@ let usuarios = [
         contrasena: 'prueba',
         dni: 45927433,
         rutaImagen: 'profesor.jpg',
+        sexo: 'Masculino',
     },
 ];
 
@@ -179,6 +184,15 @@ let alumnos = [
     },
 ];
 
+// let botonLogin = document.getElementById('botonLogin');
+// botonLogin.addEventListener('click', storage);
+// let datoLogin;
+
+// function storage() {
+//     let input = document.getElementById('inputLogin').value;
+//     datoLogin = localStorage.setItem('usuarioGuardado', input);
+// }
+
 let navBar = document.getElementById('nav');
 let user = document.getElementById('usuario');
 let usuarioIngresado = prompt('Ingrese su apellido Ej.: Perez o Torres');
@@ -200,62 +214,116 @@ usuarios.forEach((usuario) => {
 });
 
 let contenedor = document.getElementById('contenedor');
-contenedor.innerHTML = '';
+inicio();
+
+function inicio() {
+    usuarios.forEach((usuario) => {
+        if (usuario.usuario.toLowerCase() === usuarioIngresado.toLowerCase()) {
+            if (usuario.sexo == 'Femenino') {
+                contenedor.innerHTML = `
+                <h1 class="inicio">Bienvenida ${usuario.nombre}</h1>
+                `;
+            } else {
+                contenedor.innerHTML = `
+                <h1 class="inicio">Bienvenido ${usuario.nombre}</h1>
+                `;
+            }
+        }
+    });
+}
 
 let botonLista = document.getElementById('botonLista');
-botonLista.addEventListener('click', listaAlumnos);
+botonLista.addEventListener('click', () => listaAlumnos(alumnos));
 
-function listaAlumnos() {
-    contenedor.innerHTML = '';
-    alumnos.forEach((alumno) => {
+function listaAlumnos(array) {
+    contenedor.innerHTML = `
+        <div class="contenedorLista">
+            <div class="filtros">
+                <input type="text" id="buscador">
+                <label><input id="filtroPromocion" type="checkbox">Promocion</label>
+                <label><input id="filtroRegular" type="checkbox">Regular</label>
+                <label><input id="filtroLibre" type="checkbox">Libre</label>
+            </div>
+            <div id="linea1">
+                <h4 class = "cabecera">LEGAJO</h4>
+                <h4 class = "cabecera">NOMBRE</h4>
+                <h4 class = "cabecera">APELLIDO</h4>
+                <h4 class = "cabecera">TP1</h4>
+                <h4 class = "cabecera">TP2</h4>
+                <h4 class = "cabecera">TP3</h4>
+                <h4 class = "cabecera">TP4</h4>
+                <h4 class = "cabecera">PARCIAL 1</h4>
+                <h4 class = "cabecera">PARCIAL 2</h4>
+                <h4 class = "cabecera">ESTADO</h4>
+            </div>
+            <div id="lista"></div>
+        </div>
+        `;
+
+    array.forEach((elemento) => {
         let tarjetaAlumno = document.createElement('div');
 
         tarjetaAlumno.classList.add('tarjetaAlumno');
         tarjetaAlumno.innerHTML = `
-            <p class = 'p1'>${alumno.legajo}</p>
-            <p class = 'p2'>${alumno.nombre}</p>
-            <p class = 'p3'>${alumno.apellido}</p>
-            <p class = 'p4'>${alumno.tp1}</p>
-            <p class = 'p5'>${alumno.tp2}</p>
-            <p class = 'p6'>${alumno.tp3}</p>
-            <p class = 'p7'>${alumno.tp4}</p>
-            <p class = 'p8'>${alumno.primerParcial}</p>
-            <p class = 'p9'>${alumno.segundoParcial}</p>
+            <p class = 'p1'>${elemento.legajo}</p>
+            <p class = 'p2'>${elemento.nombre}</p>
+            <p class = 'p3'>${elemento.apellido}</p>
+            <p class = 'p4'>${elemento.tp1}</p>
+            <p class = 'p5'>${elemento.tp2}</p>
+            <p class = 'p6'>${elemento.tp3}</p>
+            <p class = 'p7'>${elemento.tp4}</p>
+            <p class = 'p8'>${elemento.primerParcial}</p>
+            <p class = 'p9'>${elemento.segundoParcial}</p>
+            <p class = 'p10'>${elemento.estado}</p>
         `;
-        contenedor.appendChild(tarjetaAlumno);
+        lista.appendChild(tarjetaAlumno);
     });
+}
+
+let buscador = document.getElementById('buscador');
+buscador.addEventListener('input', () => filtrarYRenderizar(alumnos, buscador.value));
+function filtrarYRenderizar(arrayIngresado, input) {
+    let alumnosFiltrados = arrayIngresado.filter((elemento) => elemento.nombre.toLowerCase().includes(input.toLowerCase()));
+    listaAlumnos(alumnosFiltrados);
 }
 
 let botonNuevoAlumno = document.getElementById('botonNuevoAlumno');
 let legajoNuevo = 1010;
+let nombre;
+let apellido;
+let dni;
 
-botonNuevoAlumno.addEventListener('click', nuevoAlumno);
+botonNuevoAlumno.addEventListener('click', contenedorNuevosAlumnos);
 
-function nuevoAlumno() {
-    let legajo = legajoNuevo + 1;
-    let nombre;
-    do {
-        nombre = prompt('Ingrese nombre del alumno');
-    } while (!isNaN(nombre) || !nombre);
-    let apellido;
-    do {
-        apellido = prompt('Ingrese apellido del alumno');
-    } while (!isNaN(apellido) || !apellido);
-    let dni;
-    do {
-        dni = prompt('Ingrese DNI del alumno');
-    } while (isNaN(dni) || !dni);
-    let tp1 = null;
-    let tp2 = null;
-    let tp3 = null;
-    let tp4 = null;
-    let primerParcial = null;
-    let segundoParcial = null;
+function contenedorNuevosAlumnos() {
+    contenedor.innerHTML = `
+    <div class="datosNuevoAlumno">
+        <input type="text" class="datoNuevoAlumno" id="nombreNuevoAlumno" placeholder="Ingrese nombre del alumno">
+        <input type="text" class="datoNuevoAlumno" id="apellidoNuevoAlumno" placeholder="Ingrese apellido del alumno">
+        <input type="number" class="datoNuevoAlumno" id="dniNuevoAlumno" placeholder="Ingrese DNI del alumno">
+        <button class="botonMenu" id="botonCargarNuevoAlumno">CARGAR</button>
+    </div>
+    `;
+    let botonCargarNuevoAlumno = document.getElementById('botonCargarNuevoAlumno');
+    botonCargarNuevoAlumno.addEventListener('click', nuevoAlumno);
 
-    legajoNuevo = legajo;
+    function nuevoAlumno() {
+        nombre = document.getElementById('nombreNuevoAlumno').value;
+        apellido = document.getElementById('apellidoNuevoAlumno').value;
+        dni = document.getElementById('dniNuevoAlumno').value;
+        let legajo = legajoNuevo + 1;
+        let tp1 = null;
+        let tp2 = null;
+        let tp3 = null;
+        let tp4 = null;
+        let primerParcial = null;
+        let segundoParcial = null;
+        let estado = null;
+        legajoNuevo = legajo;
 
-    alumnos.push({ legajo, nombre, apellido, dni, tp1, tp2, tp3, tp4, primerParcial, segundoParcial });
-    listaAlumnos();
+        alumnos.push({ legajo, nombre, apellido, dni, tp1, tp2, tp3, tp4, primerParcial, segundoParcial, estado });
+        listaAlumnos();
+    }
 }
 
 let botonCargarNotas = document.getElementById('botonCargarNotas');
@@ -313,9 +381,5 @@ function cargarNota() {
     listaAlumnos();
 }
 
-let botonReset = document.getElementById('botonReset');
-botonReset.addEventListener('click', reset);
-
-function reset() {
-    contenedor.innerHTML = '';
-}
+let botonInicio = document.getElementById('botonInicio');
+botonInicio.addEventListener('click', inicio);
