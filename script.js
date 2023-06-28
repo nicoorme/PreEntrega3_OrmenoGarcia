e686443a5cede9075db173e120999872af0775f7
Nicolas Ormeno <nicolasormeno@hotmail.com>
2023-06-28 18:04:49 -0300

    inputs Cargar Nota

--- script.js
+++ script.js
@@ -317,10 +317,10 @@ botonNuevoAlumno.addEventListener('click', contenedorNuevosAlumnos);
 function contenedorNuevosAlumnos() {
     contenedorInfo.innerHTML = `
         <div class="datosNuevoAlumno">
-            <input type="text" class="datoNuevoAlumno" id="nombreNuevoAlumno" placeholder="Ingrese nombre del alumno">
-            <input type="text" class="datoNuevoAlumno" id="apellidoNuevoAlumno" placeholder="Ingrese apellido del alumno">
-            <input type="number" class="datoNuevoAlumno" id="dniNuevoAlumno" placeholder="Ingrese DNI del alumno">
-            <button class="botonMenu" id="botonCargarNuevoAlumno">CARGAR</button>
+            <input type="text" class="input" id="nombreNuevoAlumno" placeholder="Ingrese nombre del alumno">
+            <input type="text" class="input" id="apellidoNuevoAlumno" placeholder="Ingrese apellido del alumno">
+            <input type="number" class="input" id="dniNuevoAlumno" placeholder="Ingrese DNI del alumno">
+            <button class="botonCargar" id="botonCargarNuevoAlumno">CARGAR</button>
         </div>
     `;
     let botonCargarNuevoAlumno = document.getElementById('botonCargarNuevoAlumno');
@@ -353,135 +353,288 @@ let botonCargarNotas = document.getElementById('botonCargarNotas');
 botonCargarNotas.addEventListener('click', cargarNota);
 
 function cargarNota() {
-    let legajoElegido = prompt('Elegi nro de legajo:');
-    const alumnoNotas = alumnos.find((alumno) => alumno.legajo == legajoElegido);
+    contenedorInfo.innerHTML = `
+        <div class="datosNuevoAlumno">
+        <input class="input" id="inputLegajo" placeholder="Nro Legajo Alumno">
+        <input class="input" id="inputTP1" placeholder="Nota TP1 o Ausente">
+        <input class="input" id="inputTP2" placeholder="Nota TP2 o Ausente">
+        <input class="input" id="inputTP3" placeholder="Nota TP3 o Ausente">
+        <input class="input" id="inputTP4" placeholder="Nota TP4 o Ausente">
+        <input class="input" id="inputPrimerParcial" placeholder="Nota Primer Parcial o Ausente">
+        <input class="input" id="inputSegundoParcial" placeholder="Nota Segundo Parcial o Ausente">
+        <button class="botonCargar" id="cargarNota">CARGAR</button>
+        </div>
+    `;
+    let inputLegajo = document.getElementById('inputLegajo');
+    let inputTP1 = document.getElementById('inputTP1');
+    let inputTP2 = document.getElementById('inputTP2');
+    let inputTP3 = document.getElementById('inputTP3');
+    let inputTP4 = document.getElementById('inputTP4');
+    let inputPrimerParcial = document.getElementById('inputPrimerParcial');
+    let inputSegundoParcial = document.getElementById('inputSegundoParcial');
 
-    let tp1;
-    let dato1;
-    dato1 = prompt('Ingrese nota de tp1 o Ausente');
-    if (!dato1) {
-        tp3 = 'Sin datos';
-    } else if (dato1 == 'ausente') {
-        tp1 = 1;
-    } else {
-        tp1 = dato1;
+    let botonCargarNotas = document.getElementById('cargarNota');
+    botonCargarNotas.addEventListener('click', nuevaNota);
+
+    function nuevaNota() {
+        let legajoElegido = inputLegajo.value;
+        const alumnoNotas = alumnos.find((alumno) => alumno.legajo == legajoElegido);
+
+        let tp1;
+        let dato1 = inputTP1.value;
+        if (!dato1) {
+            tp3 = 'Sin datos';
+        } else if (dato1 == 'ausente') {
+            tp1 = 1;
+        } else {
+            tp1 = dato1;
+        }
+
+        let tp2;
+        let dato2 = inputTP2.value;
+        if (!dato2) {
+            tp3 = 'Sin datos';
+        } else if (dato2 == 'ausente') {
+            tp2 = 1;
+        } else {
+            tp2 = dato2;
+        }
+
+        let tp3;
+        let dato3 = inputTP3.value;
+        if (!dato3) {
+            tp3 = 'Sin datos';
+        } else if (dato3 == 'ausente') {
+            tp3 = 1;
+        } else {
+            tp3 = dato3;
+        }
+
+        let tp4;
+        let dato4 = inputTP4.value;
+        if (!dato4) {
+            tp4 = 'Sin datos';
+        } else if (dato4 == 'ausente') {
+            tp4 = 1;
+        } else {
+            tp4 = dato4;
+        }
+
+        let primerParcial;
+        let dato5 = inputPrimerParcial.value;
+        if (!dato5) {
+            primerParcial = 'Sin datos';
+        } else if (dato5 == 'ausente') {
+            primerParcial = 1;
+        } else {
+            primerParcial = dato5;
+        }
+
+        let segundoParcial;
+        let dato6 = inputSegundoParcial.value;
+        if (!dato6) {
+            segundoParcial = 'Sin datos';
+        } else if (dato6 == 'ausente') {
+            segundoParcial = 1;
+        } else {
+            segundoParcial = dato6;
+        }
+
+        alumnoNotas.tp1 = tp1;
+        alumnoNotas.tp2 = tp2;
+        alumnoNotas.tp3 = tp3;
+        alumnoNotas.tp4 = tp4;
+        alumnoNotas.primerParcial = primerParcial;
+        alumnoNotas.segundoParcial = segundoParcial;
+
+        let condicion1;
+        if (tp1 >= 7) {
+            condicion1 = 1;
+        } else {
+            condicion1 = 0;
+        }
+
+        let condicion2;
+        if (tp2 >= 7) {
+            condicion2 = 1;
+        } else {
+            condicion2 = 0;
+        }
+
+        let condicion3;
+        if (tp3 >= 7) {
+            condicion3 = 1;
+        } else {
+            condicion3 = 0;
+        }
+
+        let condicion4;
+        if (tp4 >= 7) {
+            condicion4 = 1;
+        } else {
+            condicion4 = 0;
+        }
+
+        let sumaTP = condicion1 + condicion2 + condicion3 + condicion4;
+        let condicionTP;
+
+        console.log(condicion1);
+        console.log(condicion2);
+        console.log(condicion3);
+        console.log(condicion4);
+
+        if (sumaTP == 4) {
+            condicionTP = 2;
+        } else if (sumaTP == 3) {
+            condicionTP = 1;
+        } else {
+            condicionTP = 0;
+        }
+
+        if (alumnoNotas.primerParcial >= 7 && alumnoNotas.segundoParcial >= 7) {
+            alumnoNotas.estado = 'APROBADO';
+        } else if (condicionTP == 2 && alumnoNotas.primerParcial >= 5 && alumnoNotas.segundoParcial >= 5) {
+            alumnoNotas.estado = 'APROBADO';
+        } else if (alumnoNotas.primerParcial < 5 || alumnoNotas.segundoParcial < 5) {
+            alumnoNotas.estado = 'LIBRE';
+        } else {
+            alumnoNotas.estado = 'REGULAR';
+        }
+
+        listaAlumnos(alumnos);
     }
-
-    let tp2;
-    let dato2;
-    dato2 = prompt('Ingrese nota de tp2 o Ausente');
-    if (!dato2) {
-        tp3 = 'Sin datos';
-    } else if (dato2 == 'ausente') {
-        tp2 = 1;
-    } else {
-        tp2 = dato2;
-    }
-
-    let tp3;
-    let dato3;
-    dato3 = prompt('Ingrese nota de tp3 o Ausente');
-    if (!dato3) {
-        tp3 = 'Sin datos';
-    } else if (dato3 == 'ausente') {
-        tp3 = 1;
-    } else {
-        tp3 = dato3;
-    }
-
-    let tp4;
-    let dato4;
-    dato4 = prompt('Ingrese nota de tp4 o Ausente');
-    if (!dato4) {
-        tp4 = 'Sin datos';
-    } else if (dato4 == 'ausente') {
-        tp4 = 1;
-    } else {
-        tp4 = dato4;
-    }
-
-    let primerParcial;
-    let dato5;
-    dato5 = prompt('Ingrese nota de primer parcial o Ausente');
-    if (!dato5) {
-        primerParcial = 'Sin datos';
-    } else if (dato5 == 'ausente') {
-        primerParcial = 1;
-    } else {
-        primerParcial = dato5;
-    }
-
-    let segundoParcial;
-    let dato6;
-    dato6 = prompt('Ingrese nota del segundo parcial o Ausente');
-    if (!dato6) {
-        segundoParcial = 'Sin datos';
-    } else if (dato6 == 'ausente') {
-        segundoParcial = 1;
-    } else {
-        segundoParcial = dato6;
-    }
-
-    alumnoNotas.tp1 = tp1;
-    alumnoNotas.tp2 = tp2;
-    alumnoNotas.tp3 = tp3;
-    alumnoNotas.tp4 = tp4;
-    alumnoNotas.primerParcial = primerParcial;
-    alumnoNotas.segundoParcial = segundoParcial;
-
-    let condicion1;
-    if (tp1 >= 7) {
-        condicion1 = 1;
-    } else {
-        condicion1 = 0;
-    }
-
-    let condicion2;
-    if (tp2 >= 7) {
-        condicion2 = 1;
-    } else {
-        condicion2 = 0;
-    }
-
-    let condicion3;
-    if (tp3 >= 7) {
-        condicion3 = 1;
-    } else {
-        condicion3 = 0;
-    }
-
-    let condicion4;
-    if (tp4 >= 7) {
-        condicion4 = 1;
-    } else {
-        condicion4 = 0;
-    }
-
-    let sumaTP = condicion1 + condicion2 + condicion3 + condicion4;
-    let condicionTP;
-
-    console.log(condicion1);
-    console.log(condicion2);
-    console.log(condicion3);
-    console.log(condicion4);
-
-    if (sumaTP == 4) {
-        condicionTP = 2;
-    } else if (sumaTP == 3) {
-        condicionTP = 1;
-    } else {
-        condicionTP = 0;
-    }
-
-    if (alumnoNotas.primerParcial >= 7 && alumnoNotas.segundoParcial >= 7) {
-        alumnoNotas.estado = 'APROBADO';
-    } else if (condicionTP == 2 && alumnoNotas.primerParcial >= 5 && alumnoNotas.segundoParcial >= 5) {
-        alumnoNotas.estado = 'APROBADO';
-    } else if (alumnoNotas.primerParcial < 5 || alumnoNotas.segundoParcial < 5) {
-        alumnoNotas.estado = 'LIBRE';
-    } else {
-        alumnoNotas.estado = 'REGULAR';
-    }
-
-    listaAlumnos(alumnos);
 }
+
+// function cargarNota() {
+//     let legajoElegido = prompt('Elegi nro de legajo:');
+//     const alumnoNotas = alumnos.find((alumno) => alumno.legajo == legajoElegido);
+
+//     let tp1;
+//     let dato1;
+//     dato1 = prompt('Ingrese nota de tp1 o Ausente');
+//     if (!dato1) {
+//         tp3 = 'Sin datos';
+//     } else if (dato1 == 'ausente') {
+//         tp1 = 1;
+//     } else {
+//         tp1 = dato1;
+//     }
+
+//     let tp2;
+//     let dato2;
+//     dato2 = prompt('Ingrese nota de tp2 o Ausente');
+//     if (!dato2) {
+//         tp3 = 'Sin datos';
+//     } else if (dato2 == 'ausente') {
+//         tp2 = 1;
+//     } else {
+//         tp2 = dato2;
+//     }
+
+//     let tp3;
+//     let dato3;
+//     dato3 = prompt('Ingrese nota de tp3 o Ausente');
+//     if (!dato3) {
+//         tp3 = 'Sin datos';
+//     } else if (dato3 == 'ausente') {
+//         tp3 = 1;
+//     } else {
+//         tp3 = dato3;
+//     }
+
+//     let tp4;
+//     let dato4;
+//     dato4 = prompt('Ingrese nota de tp4 o Ausente');
+//     if (!dato4) {
+//         tp4 = 'Sin datos';
+//     } else if (dato4 == 'ausente') {
+//         tp4 = 1;
+//     } else {
+//         tp4 = dato4;
+//     }
+
+//     let primerParcial;
+//     let dato5;
+//     dato5 = prompt('Ingrese nota de primer parcial o Ausente');
+//     if (!dato5) {
+//         primerParcial = 'Sin datos';
+//     } else if (dato5 == 'ausente') {
+//         primerParcial = 1;
+//     } else {
+//         primerParcial = dato5;
+//     }
+
+//     let segundoParcial;
+//     let dato6;
+//     dato6 = prompt('Ingrese nota del segundo parcial o Ausente');
+//     if (!dato6) {
+//         segundoParcial = 'Sin datos';
+//     } else if (dato6 == 'ausente') {
+//         segundoParcial = 1;
+//     } else {
+//         segundoParcial = dato6;
+//     }
+
+//     alumnoNotas.tp1 = tp1;
+//     alumnoNotas.tp2 = tp2;
+//     alumnoNotas.tp3 = tp3;
+//     alumnoNotas.tp4 = tp4;
+//     alumnoNotas.primerParcial = primerParcial;
+//     alumnoNotas.segundoParcial = segundoParcial;
+
+//     let condicion1;
+//     if (tp1 >= 7) {
+//         condicion1 = 1;
+//     } else {
+//         condicion1 = 0;
+//     }
+
+//     let condicion2;
+//     if (tp2 >= 7) {
+//         condicion2 = 1;
+//     } else {
+//         condicion2 = 0;
+//     }
+
+//     let condicion3;
+//     if (tp3 >= 7) {
+//         condicion3 = 1;
+//     } else {
+//         condicion3 = 0;
+//     }
+
+//     let condicion4;
+//     if (tp4 >= 7) {
+//         condicion4 = 1;
+//     } else {
+//         condicion4 = 0;
+//     }
+
+//     let sumaTP = condicion1 + condicion2 + condicion3 + condicion4;
+//     let condicionTP;
+
+//     console.log(condicion1);
+//     console.log(condicion2);
+//     console.log(condicion3);
+//     console.log(condicion4);
+
+//     if (sumaTP == 4) {
+//         condicionTP = 2;
+//     } else if (sumaTP == 3) {
+//         condicionTP = 1;
+//     } else {
+//         condicionTP = 0;
+//     }
+
+//     if (alumnoNotas.primerParcial >= 7 && alumnoNotas.segundoParcial >= 7) {
+//         alumnoNotas.estado = 'APROBADO';
+//     } else if (condicionTP == 2 && alumnoNotas.primerParcial >= 5 && alumnoNotas.segundoParcial >= 5) {
+//         alumnoNotas.estado = 'APROBADO';
+//     } else if (alumnoNotas.primerParcial < 5 || alumnoNotas.segundoParcial < 5) {
+//         alumnoNotas.estado = 'LIBRE';
+//     } else {
+//         alumnoNotas.estado = 'REGULAR';
+//     }
+
+//     listaAlumnos(alumnos);
+// }
